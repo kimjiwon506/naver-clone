11d0a62bb4a80f826b7f53d46f3c9f3a350692c2
DESKTOP-P8G1MPM\Withbrother_GN_02 <jiwon@withbrother.co.kr>
2022-11-30 19:48:06 +0900

    애니메이션 추가하기

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,34 @@
-const topBanner = $(".close_button");
-const weatherBanner = $(".weather_group");
+$(document).ready(function () {
+  const topBanner = $(".close_button");
 
-$(".close_button").click(function () {
-  $(".top_banner").addClass("active");
+  topBanner.click(function () {
+    topBanner.addClass("active");
+  });
+
+  $(".weather_group").not(".active").hide();
+  setInterval(nextSlide, 3000);
+
+  function nextSlide() {
+    $(".weather_group").hide();
+    var allSlide = $(".weather_group");
+    var currentIndex = 0;
+    $(".weather_group").each(function (index, item) {
+      if ($(this).hasClass("active")) {
+        currentIndex = index;
+      }
+    });
+
+    var newIndex = 0;
+
+    if (currentIndex >= allSlide.length - 1) {
+      newIndex = 0;
+    } else {
+      newIndex = currentIndex + 1;
+    }
+
+    $(".weather_group").removeClass("active");
+
+    $(".weather_group").eq(newIndex).addClass("active");
+    $(".weather_group").eq(newIndex).show();
+  }
 });
-
-setInterval(function () {
-  // topBanner.animate({right : "-800px"}, 400, function(){
-
-  // })
-  weatherBanner.animate({ top: "-100px" });
-}, 1000);
